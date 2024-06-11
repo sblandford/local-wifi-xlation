aeea72b2c3018dc2a893cb620a2ae8b0db25319c
Simon Blandford <home@simonb.org.uk>
2024-06-11 16:07:22 +0100

    Basic event handling

---
+++ html/js/xlation.js
@@ -0,0 +1,26 @@
+
+window.onload = function () {
+
+    let txObjs = document.getElementsByClassName("txIconDiv");
+    attachEventListeners(txObjs);
+    let rxObjs = document.getElementsByClassName("channelGridTd");
+    attachEventListeners(rxObjs);
+
+}
+
+function attachEventListeners(objs) {
+    Array.prototype.forEach.call(objs, function (obj) {
+        obj.addEventListener("click", function(e) {
+            let id = this.id.substring(0,2);
+            let chan = this.id.substring(2);
+            clickEvent(id, chan, e);
+        });
+    });
+}
+
+function clickEvent(id, chan, e) {
+    if (id == "tx") {
+        e.stopPropagation();
+    }
+    console.log("Function : " + id + ", chan : " + chan)
+}
