26094d672b1bdbfc5cb59bb4799d9f2ac73040f8
Simon Blandford <home@simonb.org.uk>
2024-06-26 11:22:50 +0100

    Start status fetch and locking

--- aws-lambda/local-wifi-xlation-webrtc-offer-answer/lambda_function.py
+++ aws-lambda/local-wifi-xlation-webrtc-offer-answer/lambda_function.py
@@ -3,15 +3,23 @@ import os
 import uuid
 import ssl
 import ipaddress
+import uuid
 from pymemcache.client.base import Client
 
 context = ssl.create_default_context()
 
 memcached_client = Client((os.environ['ELASTICACHE_URL'], os.environ['ELASTICACHE_PORT']), tls_context=context)
 
+def is_valid_uuid(val):
+    try:
+        uuid.UUID(str(val))
+        return True
+    except ValueError:
+        return False
 
 def lambda_handler(event, context):
-    status_code = None
+    status_code = 500
+    status_headline = 'FAIL'
     message = None
     key = 'none'
 
@@ -24,27 +32,48 @@ def lambda_handler(event, context):
             ip = str(ipaddress.ip_network(ip + '/64', strict=False))
 
         if not 'queryStringParameters' in event:
-            raise Exception("No query parameters: 'status', 'chan', 'client' or 'direction'")
-            
+            status_code = 400
+            raise Exception("No query parameters: 'status', 'lock', 'chan', 'client' or 'direction'")
+
         if not 'chan' in event['queryStringParameters']:
+            status_code = 400
             raise Exception("Required 'chan' query parameter not specified")
         channel = event['queryStringParameters']['chan']
 
         if 'status' in event['queryStringParameters']:
+            if not 'lock' in event['queryStringParameters']:
+                status_code = 400
+                raise Exception("Required 'lock' query parameter not specified with status")
             key = str(channel) + '_' + ip
+            lock = event['queryStringParameters']['lock']
+            if not is_valid_uuid(lock):
+                status_code = 400
+                raise Exception("'lock' query parameter is not a valid UUID")
+            result = memcached_client.get(key + 'lock')
+            if result is not None and result.decode("utf-8") != lock:
+                status_code = 400
+                status_headline = 'LOCKED'
+                raise Exception("'lock' query parameter is not the same UUID as lock requestor")
+            # Create a lock to prevent other clients of messing with the status until it is written back or times out
+            memcached_client.set(key + 'lock', lock, expire=int(os.environ['ELASTICACHE_LOCK_TIMEOUT']), noreply=False)
         else:
             if not 'client' in event['queryStringParameters']:
+                status_code = 400
                 raise Exception("Required 'client' query parameter not specified")
             client = event['queryStringParameters']['client']
             if not 'direction' in event['queryStringParameters']:
+                status_code = 400
                 raise Exception("Required 'direction' query parameter not specified")
             direction = event['queryStringParameters']['direction']
-    
+
             key = str(channel) + '_' + str(client) + '_' + str(direction) + '_' + ip
-        
+
         if http_method == 'POST':
             request_body = json.loads(event['body'])
             memcached_client.set(key, request_body, expire=int(os.environ['ELASTICACHE_TIMEOUT']), noreply=False)
+            # Remove the lock on the status
+            if 'status' in event['queryStringParameters']:
+                memcached_client.delete(key + 'lock')
             status_code = 200
             body = '{"status" : "OK", "key" : "' + key + '", "message" : ""}'
         elif http_method == 'GET':
@@ -60,10 +89,9 @@ def lambda_handler(event, context):
             status_code = 400
             body = '{"status" : "FAIL", "key" : "' + key + '", "message" : "Request must be POST or GET"}'
     except Exception as e:
-        status_code = 500
-        body = '{"status" : "FAIL", "key" : "' + key + '", "message" : "' + str(e) + '"}'
+        body = '{"status" : "' + status_headline + '", "key" : "' + key + '", "message" : "' + str(e) + '"}'
     finally:
         return {
             'statusCode': status_code,
             'body': body
-        }    
+        }
--- html/js/xlation.js
+++ html/js/xlation.js
@@ -1,3 +1,14 @@
+const numChans = 6;
+const cacheUrl = "https://6n2b4yjwl3y4ztk24wppxfrkvm0zrqtv.lambda-url.eu-west-2.on.aws/";
+
+const xlationStatus = Array(numChans).fill({
+    "pc": {},
+    "offer" : {},
+    "answer" : {},
+    "available" : true,
+    "tx" : false,
+    "client" : 0
+ });
 
 window.onload = function () {
 
@@ -7,6 +18,10 @@ window.onload = function () {
     let rxObjs = document.getElementsByClassName("channelGridTd");
     attachEventListeners(rxObjs);
 
+}
+
+function errHandler(err) {
+    console.log(err);
 }
 
 function attachEventListeners(objs) {
@@ -22,6 +37,73 @@ function attachEventListeners(objs) {
 function clickEvent(id, chan, e) {
     if (id == "tx") {
         e.stopPropagation();
+        tx(chan);
     }
-    console.log("Function : " + id + ", chan : " + chan)
+    rx(chan);
+}
+
+async function fetch_json(url) {
+    try {
+        const response = await fetch(url, {method: 'GET', headers: {'Accept': 'application/json'}});
+        if (response.ok) {
+            return(await response.json());
+        } else {
+            throw {
+                json: await response.json(),
+                status: response.status,
+                statusText: response.statusText
+            };
+        }
+    } catch (e) {
+        if (e.json){
+            console.error(JSON.stringify(e.json));
+        } else {
+            console.error("Request err:" + e);
+        }
+    }
+}
+
+async function fetch_status(chan) {
+    const statUrl = cacheUrl + "?status&chan=" + chan;
+    const xlationStatus = await fetch_json(statUrl);
+    // Take no action if status not found
+    if (!xlationStatus || (xlationStatus.status && xlationStatus.status == "MISS")) {
+        return;
+    }
+
+
+}
+
+function tx(chan) {
+    fetch_status(chan);
+    return;
+    if (xlationStatus[chan].available) {
+        let pc = new RTCPeerConnection({});
+        xlationStatus[chan].pc = pc;
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => {
+            pc.addStream(stream);
+        }).catch(errHandler);
+        pc.onconnection = function(e) {
+            console.log('onconnection ', e);
+        }
+        pc.createOffer().then(des => {
+            console.log('createOffer ok ');
+            pc.setLocalDescription(des).then(() => {
+                setTimeout(function() {
+                    if (pc.iceGatheringState == "complete") {
+                        return;
+                    } else {
+                        console.log('after GetherTimeout');
+                        xlationStatus[chan].offer = JSON.stringify(pc.localDescription);
+                    }
+                }, 2000);
+                console.log('setLocalDescription ok');
+            }).catch(errHandler);
+        }).catch(errHandler);
+    }
+
+}
+
+function rx(chan) {
+
 }
